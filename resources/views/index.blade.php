db812501aa863d586f2df40d8a358f73319cc740
Fred-S <fredvantage@gmail.com>
2020-09-25 14:23:39 +0200

    added more content & styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,21 +11,15 @@
                     <a href="#">
                         <img src="/img/parasite.jpg" alt="parasite" class="">
                     </a>
-                </div>
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="/img/parasite.jpg" alt="parasite" class="">
-                    </a>
-                </div>
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="/img/parasite.jpg" alt="parasite" class="">
-                    </a>
-                </div>
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="/img/parasite.jpg" alt="parasite" class="">
-                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">Parasite</a>
+                        <div class="flex items-center text-gray-400">
+                            <span>star</span>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>feb 20, 2020</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
