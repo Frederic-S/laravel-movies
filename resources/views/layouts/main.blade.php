e99d90c41fe5d9bb5e45635a4622ef1354a207a6
Fred-S <fredvantage@gmail.com>
2020-09-25 07:47:17 +0200

    added search icon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -30,7 +30,12 @@
                 </ul>
                 <div class="flex items-center">
                     <div class="relative">
-                        <input type="text" class="bg-gray-800 rounded-full w-64 pl-8 px-4 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
+                        <input type="text" class="bg-gray-800 rounded-full w-64 pl-8 px-4 py-1 text-sm focus:outline-none focus:shadow-outline" placeholder="Search">
+                        <div class="absolute top-0">
+                            <svg class="w-4 text-gray-500 mt-2 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                            </svg>
+                        </div>
                     </div>
                 </div>
             </div>
