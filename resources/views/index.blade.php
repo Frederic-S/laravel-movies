6d348a0d917ea2cc71540de436c33dc5238ae0a8
Fred-S <fredvantage@gmail.com>
2020-09-25 11:41:53 +0200

    added card layout in index movies

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,33 @@
 @extends('layouts/main')
 
 @section('content')
-    <div><h1>Index of movies</h1></div>
+    <div class="container mx-auto px-4 pt-16">
+        <div class="popular-movies">
+            <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">
+                popular movies
+            </h2>
+            <div class="grid grid-cols-4 gap-8">
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt="parasite" class="">
+                    </a>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt="parasite" class="">
+                    </a>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt="parasite" class="">
+                    </a>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt="parasite" class="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
