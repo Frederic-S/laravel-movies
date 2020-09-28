5da05dac833f749135a131263524b5ccfa15365c
Fred-S <fredvantage@gmail.com>
2020-09-28 10:42:59 +0200

    added styles

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,23 +14,23 @@
     </head>
     <body class="font-sans bg-gray-900 text-white">
         <nav class="border-b border-gray-800">
-            <div class="container mx-auto px-4 flex items-center justify-between px-4 py-6">
-                <ul class="flex items-center">
+            <div class="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between py-6">
+                <ul class="flex flex-col md:flex-row items-center">
                     <li>
                         <a href="#">Logo</a>
                     </li>
-                    <li class="ml-16">
+                    <li class="md:ml-16">
                         <a class="hover:text-gray-300" href="#">Movies</a>
                     </li>
-                    <li class="ml-6">
+                    <li class="md:ml-6">
                         <a class="hover:text-gray-300" href="#">TV Shows</a>
                     </li>
-                    <li class="ml-6">
+                    <li class="md:ml-6">
                         <a class="hover:text-gray-300" href="#">Actors</a>
                     </li>
                 </ul>
-                <div class="flex items-center">
-                    <div class="relative">
+                <div class="flex flex-col md:flex-row items-center">
+                    <div class="relative mt-3 md:mt-0 ">
                         <input type="text" class="bg-gray-800 rounded-full w-64 pl-8 px-4 py-1 text-sm focus:outline-none focus:shadow-outline" placeholder="Search">
                         <div class="absolute top-0">
                             <svg class="w-4 text-gray-500 mt-2 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
